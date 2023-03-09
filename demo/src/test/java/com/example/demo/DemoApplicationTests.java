1cd631b41ae6ac252b8a72c8e3ec6e364887aea4
praprasad <127417805+praprasad@users.noreply.github.com>
2023-03-09 17:47:43 +0100

    Update DemoApplicationTests.java

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.demo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.example.demo.controller.RunnerClass;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	private RunnerClass controller;
+	
+	@Autowired
+	private MockMvc mvc;
+	
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
