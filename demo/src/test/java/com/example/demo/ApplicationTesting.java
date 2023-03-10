75294f4c081092648c545ee6e80c85b418519259
praprasad <127417805+praprasad@users.noreply.github.com>
2023-03-10 10:20:54 +0100

    mvc updated

--- demo/src/test/java/com/example/demo/ApplicationTesting.java
+++ demo/src/test/java/com/example/demo/ApplicationTesting.java
@@ -12,9 +12,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.web.servlet.MockMvc;
-
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class ApplicationTesting {
 
 	@Autowired
--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -4,20 +4,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import com.example.demo.controller.RunnerClass;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class DemoApplicationTests {
 
 	@Autowired
 	private RunnerClass controller;
-	
-	@Autowired
-	private MockMvc mvc;
 	
 	@Test
 	void contextLoads() {
